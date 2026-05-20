d45cebd0f8d0dfd8c494bd36948b12d6bc59d824
numa.tortolero <numa.tortolero@gmail.com>
2026-05-19 22:07:22 -0400

    Delete foreign files and update .gitignore

--- notes/scripts/vivid-tidal.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo Testing vivid-tidal audio and runing Vivid-Tidal Haskell interpreter. . .
-VIVIDVERSION=0.1.0.0
-# PACK=vivid-tidal-${VIVIDVERSION}
-PACK=vivid-tidal
-PACKPATH=$HOME/.local/share/haskell/${PACK}
-cd $PACKPATH
-cabal repl --repl-options="-ghci-script ${PACK}.ghci" --repl-options=-Wno-missing-home-modules
